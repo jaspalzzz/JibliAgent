86a4dc587c381818d42b01aa942550e73305465d
jaspalzzz <jaspalkumar006@gmail.com>
2022-01-26 10:06:15 +0530

    Bug fix

--- app/src/main/java/com/ssas/jibli/agent/ui/auth/SplashActivity.kt
+++ app/src/main/java/com/ssas/jibli/agent/ui/auth/SplashActivity.kt
@@ -2,11 +2,14 @@ package com.ssas.jibli.agent.ui.auth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import androidx.lifecycle.lifecycleScope
+import com.ssas.jibli.agent.MApplication
 import com.ssas.jibli.agent.R
 import com.ssas.jibli.agent.base.BaseActivitySimple
 import com.ssas.jibli.agent.data.prefs.PrefKeys
 import com.ssas.jibli.agent.ui.home.DashboardActivity
+import com.ssas.jibli.agent.utils.LanguageUtils
 import com.ssas.jibli.agent.utils.Utils
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
@@ -22,10 +25,14 @@ class SplashActivity : BaseActivitySimple() {
     private fun startSplash() {
         lifecycleScope.launch {
             delay(1500)
-            if(prefMain[PrefKeys.SALES_AGENT_CODE,""].isNullOrEmpty()){
-                Utils.jumpActivity(this@SplashActivity, LandingActivity::class.java)
-            }else{
-                Utils.jumpActivity(this@SplashActivity, DashboardActivity::class.java)
+            val language = prefMain[PrefKeys.LANGUAGE, LanguageUtils.ARABIC]
+            MApplication.language = language ?: LanguageUtils.ARABIC
+            LanguageUtils.setLanguage(this@SplashActivity, MApplication.language).run {
+                if (prefMain[PrefKeys.SALES_AGENT_CODE, ""].isNullOrEmpty()) {
+                    Utils.jumpActivity(this@SplashActivity, LandingActivity::class.java)
+                } else {
+                    Utils.jumpActivity(this@SplashActivity, DashboardActivity::class.java)
+                }
             }
             finish()
         }
